d4d36883833755b3332a0c3dcad81cb1564d811c
Thiago Gonçalves <mg.az.ul@hotmail.com>
2024-06-01 18:18:17 -0300

    Project created

---
+++ dominio_bancario/src/main/java/dev/tsantana/dominio_bancario/main.java
@@ -0,0 +1,11 @@
+package dev.tsantana.dominio_bancario;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Domínio Bancário DIO");
+	}
+
+}
